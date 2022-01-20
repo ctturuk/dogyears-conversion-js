bb585da3961ac3570b6917def843c90b6e79260b
ctturuk <ctturuk@gmail.com>
2022-01-19 19:26:32 -0700

    dog years conversion .js file

---
+++ dogyears.js
@@ -0,0 +1,22 @@
+// my age
+const myAge = 32;
+
+// early dog years
+let earlyYears = 2;
+
+earlyYears *= 10.5;
+
+// subtracts 2 from my age
+let laterYears = myAge - 2;
+
+// multiplies lateryears by 4
+laterYears *= 4;
+
+// adding early and later dog years to get my age in dog years
+let myAgeInDogYears = earlyYears + laterYears;
+
+let myName = 'jackson'.toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.
+`);
+
